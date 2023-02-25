674a79d3eca096fbcf86fe161f791f30d81e1c86
ilyashalin <ilyashalin@mail.ru>
2023-02-25 18:25:29 +0300

    C1_practice

---
+++ Practice C1/cat1.py
@@ -0,0 +1,14 @@
+from cat import Cat
+
+cat_1 = Cat("Baron", "boy", 2)
+cat_2 = Cat("Sam", "boy", 2)
+
+print(cat_1.get_name(), cat_1.get_gender(), cat_1.get_age())
+print(cat_2.get_name(), cat_2.get_gender(), cat_2.get_age())
+
+class Dog(Cat):
+    def get_pet(self):
+        return f'{self.get_name()} {self.get_age()}'
+
+dog_1=Dog("Felix","boy",2)
+print(dog_1.get_pet())
